8711a24455de13f5d7e689274a9c6ead0f67c38a
gowthamreddy <gowthamreddysomala@gmail.com>
2025-08-05 18:07:38 +0530

    Patten practice

--- Patterns/Pattern8.java
+++ Patterns/Pattern8.java
@@ -2,20 +2,19 @@ package Patterns;
 
 import java.util.Scanner;
 
-public class Pattern7 {
+public class Pattern8 {
     public static void main(String args[]) {
         System.out.println("Enter size of elements :");
         Scanner sc = new Scanner(System.in);
         int size = sc.nextInt();
-        for(int i=0;i<=size;i++){
-            for(int j=i;j<size;j++){
+        for(int i=0;i<size;i++){
+            for(int j=0;j<i;j++){
                 System.out.print(" ");
             }
-            for(int k=0;k<=i;k++){
+            for(int k=i;k<size;k++){
                 System.out.print("* ");
             }
             System.out.println();
         }
-
     }
     }
