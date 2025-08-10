38406dc6e628944bd227ecf10e816d4fa34308a7
gowthamreddy <gowthamreddysomala@gmail.com>
2025-08-10 13:19:15 +0530

    practice

--- Recursion/RecursionBasics.java
+++ Recursion/RecursionBasics.java
@@ -13,7 +13,7 @@ public class RecursionBasics {
     }
     //printing numbers 0 to until n
     public static void print(int k){
-    if(k==0){
+    if(k<=1){
         return ;
     }
     k--;
@@ -28,10 +28,37 @@ public class RecursionBasics {
         k--;
         printrev(k);
     }
+    //sum of first n numbers using recursion
+   int noc = 0;
+    public int sumofn(int k){
+        if(k==0){
+            return noc;
+        }
+        noc = noc + k;
+        k--;
+        return sumofn(k);
+    }
+    //factorial of N numbers
+    int fact = 1;
+    public int factorialofN(int n){
+        if(n ==0){
+            return fact;
+        }
+        fact = fact * n;
+        n--;
+        return factorialofN(n);
+    }
+    // check if String is palindrome of not
+    boolean valid = false;
+    public boolean palindrome(String name){
+        return false;
+    }
     public static void main(String args[]){
        RecursionBasics recursionBasics = new RecursionBasics();
        // System.out.println( recursionBasics.coder(6));
-        //  print(33);
-        printrev(43);
+         // print(33);
+        //printrev(43);
+       // System.out.println(recursionBasics.sumofn(10));
+        System.out.println(recursionBasics.factorialofN(10));
     }
 }
