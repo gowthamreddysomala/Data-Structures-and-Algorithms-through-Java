acc76b4214f86d4ec91336093224bd7201acd372
gowthamreddy <gowthamreddysomala@gmail.com>
2025-08-12 19:32:20 +0530

    practice

--- Hashing/CharcterHashing.java
+++ Hashing/CharcterHashing.java
@@ -19,12 +19,21 @@ public class CharcterHashing {
         }
         return arr[number];
     }
+    /*
+    NOTE : you can improve the space efficiency of hashed array by finding the ascii value of 'a' from table : 97
+                    and before char is ' is 96
+                    so for every character you subtract so it looks like this
+                        :  a - '
+                            :  97 - 96 = 1
+                                 so if one exist thet is stored on 1 th index of array .
+                                 so the array size for only small case will be from 0 - 26  .. EASY ..
+     */
 
     public static void main(String args[]){
         CharcterHashing charcterHashing = new CharcterHashing();
         // any String is converted to Lower case for case in-sentivitity
-        String name = "Sreevalli";
-        char chartofind = 's';
+        String name = "Gowtham Reddy";
+        char chartofind = ' ';
         System.out.println("Char count in String: "+ charcterHashing.bruteforce(name ,chartofind));
     }
 }
