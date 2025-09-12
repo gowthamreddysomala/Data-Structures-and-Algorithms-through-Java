3067ab1c7f785c38eb629dcf686a15be37fa697b
gowthamreddy <gowthamreddysomala@gmail.com>
2025-09-12 13:32:27 +0530

    hello

--- Recursion/Recursion.java
+++ Recursion/Recursion.java
@@ -1,18 +1,35 @@
 package Recursion;
 
 public class Recursion {
-    public static void sayhi(int[] count , int len){
-        System.out.println(count[len]);
-        if(len==count.length-1){
+
+    public static void sayhi(int[] arr , int start, int end  ){
+    if(start>=end){
+        return;
+    }
+    int temp = arr[start];
+    arr[start] = arr[end];
+    arr[end] = temp;
+    sayhi(arr,++start,--end);
+    }
+
+
+    public static void printarr(int arr[] , int len){
+        if(len==0){
             return;
         }
-        sayhi(count ,++len);
-        System.out.println(count[len]);
+        printarr(arr,--len);
+        System.out.print(arr[len]+" ");
+
+
+
     }
 
     public static void main(String[] args) {
         int[] arr = {10,9,8,7,6,5,4,3,2,1};
-        int l = 0;
-        sayhi(arr,l);
+        int l = arr.length-1;
+        printarr(arr,arr.length);
+        sayhi(arr,0,l);
+        System.out.println();
+       printarr(arr, arr.length  );
     }
 }
