6b203d33a9a1a7fee4f01bb00bb1775a37a7d796
gowthamreddy <gowthamreddysomala@gmail.com>
2025-09-16 18:48:11 +0530

    hello

--- Arrays/easy/CheckifArrayisSorted.java
+++ Arrays/easy/CheckifArrayisSorted.java
@@ -4,17 +4,25 @@ public class CheckifArrayisSorted {
         public static boolean checkifsored(int[] arr){
             int n = arr.length;
             for(int i=0;i<n-1;i++){
-                if(arr[i+1]>arr[i]){
-                    continue;
-                }else{
-                    return false;
-                }
+               if(arr[i+1]<arr[i]){
+                   return false;
+               }
             }
             return true;
+        }
+        public static boolean RecursiveApproach(int[] arr,int length,int current){
+            if(current>=length-1){
+                return true;
+            }
+            if(arr[current]>arr[current+1]){
+                return false;
+            }
+            return RecursiveApproach(arr,length,current+1);
         }
     public static void main(String[] args){
         int[] arr = {23,43,23,5,2,3,3532,5345,54542,34,34,534,34};
         int[] arr1 = {1,2,3,4,5,6,7,8,9};
         System.out.println("Check if sorted : "+checkifsored(arr1));
+        System.out.println("Recursive Approach :"+RecursiveApproach(arr,arr.length,0));
     }
 }
--- Arrays/easy/LargestElement.java
+++ Arrays/easy/LargestElement.java
@@ -2,23 +2,23 @@ package Arrays.easy;
 
 public class LargestElement {
     public static  int largest(int arr[]){
-        int max = arr[0];
-        int length = arr.length;
-        for(int i=0;i<length;i++){
-            if(arr[i]>max){
-                max = arr[i];
-            }
-        }
-        return max;
+       int max = Integer.MIN_VALUE;
+       int n = arr.length;
+       for(int i=0;i<n;i++){
+          if(arr[i]>max){
+              max = arr[i];
+          }
+       }
+       return max;
     }
     public static int findbig(int[] arr,int max,int len,int count){
-        if(len == count){
-            return max;
-        }
-        if(arr[count]>max){
-            max = arr[count];
-        }
-        return findbig(arr,max,len,++count);
+    if(count >= len){
+        return max;
+    }
+    if(arr[count]>max){
+        max = arr[count];
+    }
+    return findbig(arr,max,len,count+1);
     }
     public static void main(String args[]){
         int[] arr = {2,3,5,2,235,656,2312,433,23,43,346,2,5388};
