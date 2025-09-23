0f688192458414fb627361a745004a77f09af28c
gowthamreddy <gowthamreddysomala@gmail.com>
2025-09-23 11:03:01 +0530

    arrays easy

--- Arrays/easy/Movezerostostart.java
+++ Arrays/easy/Movezerostostart.java
@@ -2,7 +2,18 @@ package Arrays.easy;
 
 public class Movezerostostart {
     public static void move(int[] arr){
-
+        int pointer = arr.length-1;
+        int len = arr.length;
+        for(int i=len-1;i>=0;i--){
+            if(arr[i]!=0){
+                arr[pointer] = arr[i];
+                pointer--;
+            }
+        }
+        while(pointer>=0){
+            arr[pointer] = 0;
+            pointer--;
+        }
     }
     public static void main(String[] args){
         int[] arr={0,2,5,3,2,0,23,5,0,3};
