4b65d5cf2b7e9ce840d7321e146056fc9be74618
gowthamreddy <gowthamreddysomala@gmail.com>
2025-09-25 18:29:41 +0530

    arrays medium

--- Arrays/medium/MajorityElement.java
+++ Arrays/medium/MajorityElement.java
@@ -25,8 +25,25 @@ public class MajorityElement {
         }
         return val;
     }
+    public static int best(int arr[]){
+        int len = arr.length;
+        int can = 0;
+        int vote =0;
+        for(int i=0;i<len;i++){
+            if(vote==0){
+                can = arr[i];
+                vote = 1;
+            }
+           else if(arr[i]==can){
+                vote++;
+            }else{
+                vote--;
+            }
+        }
+        return can;
+    }
     public static void main(String[] args){
         int[] arr = {3,2,4,2,5,4,4,4,4,4,6,234,643,23,4,4,4,4,4};
-        System.out.println(majorelement(arr));
+        System.out.println(best(arr));
     }
 }
