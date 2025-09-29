d1e301acd079d18ea950c90a14d2b2353a7c2312
gowthamreddy <gowthamreddysomala@gmail.com>
2025-09-29 13:22:41 +0530

    arrays medium

--- Sorting/BinarySearchAlgo.java
+++ Sorting/BinarySearchAlgo.java
@@ -1,6 +1,24 @@
 package Sorting;
 
 public class BinarySearchAlgo {
-
-
+    public static int binarysearch(int[] arr,int value,int start,int end){
+      if(end>=start){
+          int mid = start + (end-start)/2;
+          if(arr[mid]==value){
+              return mid;
+          }
+          if(arr[mid]>value){
+             return binarysearch(arr,value,start,mid-1);
+          }else{
+             return binarysearch(arr,value,mid+1,end);
+          }
+      }
+      return -1;
+    }
+    public static void main(String[] args){
+    int[] arr = {34,2,34,4,5,45,6,5673,534,6,563,5,4,52,35,3,4,2,4,523,5,432};
+    int x = 432;
+    int y = binarysearch(arr,x,0,arr.length-1);
+        System.out.println(y);
+    }
 }
