14bd5357e0cc2f3f7300f684739533f29bb28e3c
gowthamreddy <gowthamreddysomala@gmail.com>
2025-10-10 19:21:36 +0530

    arrays medium

--- Arrays/medium/ProductOfArrayExceptSelf.java
+++ Arrays/medium/ProductOfArrayExceptSelf.java
@@ -3,28 +3,11 @@ package Arrays.medium;
 public class ProductOfArrayExceptSelf {
     public static int[] product(int[] arr){
         int len = arr.length;
-        int[] temp = new int[len];
-        for(int i = 0;i<len;i++){
-            temp[i] = arr[i];
-        }
-
-
-        int temps = 1;
+        int temp = 1;
         for(int i=0;i<len;i++){
-            if(arr[i]<0){
-                temp[i] =0;
-                continue;
-            }
-            for(int j=0;j<len;j++){
-                if(i==j){
-                    continue;
-                }
-                temps = temps * arr[j];
-            }
-            temp[i] = temps;
-            temps = 1;
+
         }
-        return temp;
+        return arr;
     }
     public static void print(int[] arr){
         int len = arr.length;
--- Arrays/medium/SlidingWindow.java
+++ Arrays/medium/SlidingWindow.java
@@ -1,4 +1,58 @@
 package Arrays.medium;
 
 public class SlidingWindow {
+    public static int max_size(int[] arr,int size){
+        int len = arr.length;
+        int max = 0;
+        // finding the initial size
+        for(int i=0;i<size;i++){
+            max = max+arr[i];
+        }
+        // finding the remaining size
+        for(int i=size;i<len;i++){
+            int temp = max;
+            temp = temp - arr[i-size];
+            temp = temp + arr[i];
+            if(temp>max){
+                max = temp;
+            }
+        }
+        return max;
+    }
+    public static float averageofmaxwindow(int[] arr,int size){
+        int len = arr.length;
+        float max = 0;
+        if(size<=0 || len<2){
+            return -1;
+        }
+        for(int i=0;i<size;i++){
+            max = max + arr[i];
+        }
+        float temp = 0;
+        for(int i=size;i<len;i++){
+            temp = max;
+            temp = temp - arr[i-size];
+            temp = temp + arr[i];
+            if(temp>max){
+                max = temp;
+            }
+        }
+        return max/size;
+    }
+    public static int patternsanagram(String string,String pattern) {
+        int max = 0;
+        StringBuilder sb = new StringBuilder(pattern);
+        sb.reverse();
+        String k = sb.toString();
+        System.out.println(k);
+        return max;
+    }
+    public static void main(String[] args){
+        int[] arr = {1,2,3,4,5,6};
+        int max = max_size(arr,3);
+        System.out.println("Max size sum : "+ max);
+        float avg = averageofmaxwindow(arr,4);
+        System.out.println("Average of Window is : "+ avg);
+        System.out.println("Anagram : "+ patternsanagram("gowtham","go"));
+    }
 }
